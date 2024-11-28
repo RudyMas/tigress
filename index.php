fd239c39f10c04422091badbb338c2a914dc421c
RudyMasGoNext <rudy.mas@go-next.be>
2024-11-28 13:54:35 +0100

    Updated to PHP 8.4

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Tigress (PHP version 8.3)
+ * Tigress (PHP version 8.4)
  * Unleash the Power of PHP (MVC Framework)
  *
  * @author      Rudy Mas <rudy.mas@rudymas.be>
  * @copyright   2024, rudymas.be. (https://rudymas.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     1.0.0
+ * @version     2024.11.28.0
  */
 
 use Tigress\Core;
@@ -14,5 +14,5 @@ use Tigress\Core;
 session_start();
 require_once('vendor/autoload.php');
 
-const TIGRESS_VERSION = '1.1.2';
+const TIGRESS_VERSION = '2024.11.28';
 $Core = new Core();
