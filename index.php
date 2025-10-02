95a147deca4c0e577b3b7c9f3131b81399265b62
RudyMasGoNext <rudy.mas@go-next.be>
2025-10-02 12:00:08 +0200

    Version 2025.10.02

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @author      Rudy Mas <rudy.mas@rudymas.be>
  * @copyright   2024-2025, rudymas.be. (https://rudymas.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     2025.08.28.0
+ * @version     2025.10.02.0
  */
 
 use Tigress\Core;
@@ -14,5 +14,5 @@ use Tigress\Core;
 session_start();
 require_once('vendor/autoload.php');
 
-const TIGRESS_VERSION = '2025.08.28';
+const TIGRESS_VERSION = '2025.10.02';
 $Core = new Core();
