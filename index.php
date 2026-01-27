148dbfe03c6ee74aac229f227125d44460a0c903
RudyMasGoNext <rudy.mas@go-next.be>
2026-01-27 15:19:26 +0100

    Update packages

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
  * Unleash the Power of PHP (MVC Framework)
  *
  * @author      Rudy Mas <rudy.mas@rudymas.be>
- * @copyright   2024-2025, rudymas.be. (https://rudymas.be/)
+ * @copyright   2024-2026, rudymas.be. (https://rudymas.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     2025.12.09.0
+ * @version     2026.01.27.0
  */
 
 use Tigress\Core;
@@ -14,5 +14,5 @@ use Tigress\Core;
 session_start();
 require_once('vendor/autoload.php');
 
-const TIGRESS_VERSION = '2025.12.09';
+const TIGRESS_VERSION = '2026.01.27';
 $Core = new Core();
