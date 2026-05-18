b3e453cd37cdbd0838252269fb3c7fbc50beb6a4
RudyMasGoNext <rudy.mas@go-next.be>
2026-05-18 12:04:42 +0200

    Update packages

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @author      Rudy Mas <rudy.mas@rudymas.be>
  * @copyright   2024-2026, rudymas.be. (https://rudymas.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     2026.03.02.2
+ * @version     2026.05.18.0
  */
 
 use Tigress\Core;
@@ -14,7 +14,7 @@ use Tigress\Core;
 session_start();
 require_once('vendor/autoload.php');
 
-const TIGRESS_VERSION = '2026.03.02';
+const TIGRESS_VERSION = '2026.05.18';
 try {
     $Core = new Core();
 } catch (Throwable $e) {
