c769f812ed84f801d9ecadaff03e1ab7a2ca4d8c
RudyMasGoNext <rudy.mas@go-next.be>
2026-05-18 13:55:14 +0200

    Added npmjs package pdfmake

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
  * @author      Rudy Mas <rudy.mas@rudymas.be>
  * @copyright   2024-2026, rudymas.be. (https://rudymas.be/)
  * @license     https://opensource.org/licenses/GPL-3.0 GNU General Public License, version 3 (GPL-3.0)
- * @version     2026.05.18.0
+ * @version     2026.05.18.1
  */
 
 use Tigress\Core;
